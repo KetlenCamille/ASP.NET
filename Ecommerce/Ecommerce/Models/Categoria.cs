12709eb7f59451ef9cfade150325c560102f08f6
KetlenCamille <ketlen.rieper@gmail.com>
2018-07-29 23:09:37 -0300

    Criando a classe Categoria

---
+++ Ecommerce/Ecommerce/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class Categoria
+    {
+        public int idCategoria { get; set; }
+
+        public string descricaoCategoria { get; set; }
+    }
+}
